5ff789d9390980896c377caf9af64d5cb7c33b5b
Roberto <rbrtarenhart@gmail.com>
2023-05-07 16:10:02 -0300

    3.2 - Logica de programacao

--- app/src/main/java/com/example/primeiraapp/MainActivity.kt
+++ app/src/main/java/com/example/primeiraapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.primeiraapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.primeiraapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonOla.setOnClickListener {
+            val nome = binding.editNome.text.toString()
+//            binding.textResultado.text = "Ola " + nome
+//            binding.textResultado.text = "Ola ${nome}"
+            binding.textResultado.setText("Ola " + nome)
+        }
     }
 }
